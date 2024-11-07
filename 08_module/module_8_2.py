1ec4d8f4513a878378fb7eda27de46b6fb956835
martsenef <123342502+martsenef@users.noreply.github.com>
2024-11-07 23:49:02 +0300

    Module 08

---
+++ 08_module/module_8_2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+def personal_sum(numbers):
+	result=0
+	incorrect_data=0
+	for i in numbers:
+		try:
+			result+=i
+		except TypeError:
+			incorrect_data+=1
+			print(f"Некорректный тип данных для подсчёта суммы - {type(i)} {i}")
+	return (result,incorrect_data)
+
+def calculate_average(numbers):
+	try:
+		res,i=personal_sum(numbers)
+		return res/(len(numbers)-i)
+	except ZeroDivisionError:
+		return 0
+	except TypeError:
+		print(f"В numbers записан некорректный тип данных {type(numbers)}")
+	except Exception as e:
+		print(e)
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
